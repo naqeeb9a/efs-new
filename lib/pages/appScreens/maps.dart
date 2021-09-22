174efdb7d0a087276785dfeeec28617547c395aa
hbsgujjar111 <hbsgujjar111h@gmail.com>
2021-09-22 10:40:26 +0500

    hassan ios

--- lib/pages/appScreens/maps.dart
+++ lib/pages/appScreens/maps.dart
@@ -37,8 +37,6 @@ class _MapsState extends State<Maps> {
                     LatLng(double.parse(widget.lat), double.parse(widget.lng)),
                 zoom: 14.5),
             onMapCreated: (GoogleMapController controller) {
-              // _controller.complete(controller);
-
               if (!_controller.isCompleted) {
                 _controller.complete(controller);
               } else {}
